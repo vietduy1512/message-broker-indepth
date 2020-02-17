7c4744ce4eca697dcc951e0a7ed40a10cecc71ef
Duy <vietduy1512@gmail.com>
2020-02-17 18:43:37 +0700

    Implement work queue
    - Using concurrently package
    - Implement running with 2 commands
    - Using Round-robin dispatching

---
+++ rabbitmq/work_queue/worker.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error, connection) {
+    connection.createChannel(function(error, channel) {
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function() {
+                console.log(" [x] Done");
+            }, secs * 1000);
+        }, {
+            noAck: true
+        });
+    });
+});
